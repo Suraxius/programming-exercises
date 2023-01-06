8cb8e07832d6819dbc9951a887eae48732eacadb
Sven Luethi <dev@svenluthi.com>
2023-01-06 21:13:04 +0100

    update

--- c/analyzer.c
+++ c/analyzer.c
@@ -1,26 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-extern int add(int a, int b);
+extern int echo(int a);       // Test 1
+extern int add(int a, int b); // Test 2
 
 unsigned test1() {
-    int a = random()%99;
-    int b = random()%99;
+    int a = rand() % 99 + 1;
+    int result = echo(a);
+    if(a == result) {
+        printf("Test 1 - Completed\n");
+        return 1;
+    }
+    else {
+        printf("--------------------------------------------------------------------------------\n");
+        printf("Test 1 - Testing your int echo(a)\n");
+        printf("echo(%i) | expected result: %i, Actual result: %i\n",a,a,result);
+        printf("--------------------------------------------------------------------------------\n");
+        return 0;
+    }
+}
+
+unsigned test2() {
+    int a = rand() % 99 + 1;
+    int b = rand() % 99 + 1;
     int result = add(a,b);
-    printf("--------------------------------------------------------------------------------\n");
-    printf("Testing your int add(a,b)\n");
-    printf("add(%i, %i) | expected result: %i, Actual result: %i\n",a,b,a+b,result);
-    printf("--------------------------------------------------------------------------------\n");
-    if(a+b == result) return 1;
-    else return 0;
+
+    if(a+b == result) {
+        printf("Test 2 - Completed\n");
+        return 1;
+    }
+    else {
+        printf("--------------------------------------------------------------------------------\n");
+        printf("Test 2 - Testing your int add(a,b)\n");
+        printf("add(%i, %i) | expected result: %i, Actual result: %i\n",a,b,a+b,result);
+        printf("--------------------------------------------------------------------------------\n");
+        return 0;
+    }
 }
 
 int main(void) {
     unsigned rc = 0;
+    srand(time(NULL));
 
-    printf("Running tests on your functions...\n");
+    printf("\n\nRunning tests on your functions...\n\n");
 
     if( !test1() ) ++rc;
+    if( !test2() ) ++rc;
 
 
     printf("\n================================================================================\n");
--- c/exam.c
+++ c/exam.c
@@ -1,3 +1,17 @@
+
+// Exercise 1:
+// Create a function that spits out the same number it takes in.
+//
+int echo(int a) {
+    return 0;
+}
+
+
+
+// Exercise 2:
+// Create a function that takes in 2 variables a and b.
+// The function should add up a and b and return the result.
+//
 int add(int a, int b) {
     return 0;
 }
